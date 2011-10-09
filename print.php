925c72d2ffd6893c4e948e1873ff71ddb6e6869e
Justin Rainbow <justin.rainbow@gmail.com>
2011-10-09 16:48:41 -0700

    Adding logging to the print.php script

--- print.php
+++ print.php
@@ -3,8 +3,9 @@
 
 $cfg = json_decode(file_get_contents(__DIR__.'/config.json'), true);
 
-$file = $argv[1];
+$file = __DIR__.'/files/strips/'.$argv[1].'.jpg';
 
 if (file_exists($file)) {
-    exec(sprintf('/usr/bin/lp -d "%s" -o page-top=0 -o page-bottom=0 -o scaling=100 %s', $cfg['printer'], escapeshellarg($file)));
+  exec(sprintf('/usr/bin/lp -d "%s" -o page-top=0 -o page-bottom=0 -o scaling=100 %s', $cfg['printer'], escapeshellarg($file)), $output);
+  file_put_contents(__DIR__.'/printer.log', json_encode($output)."\n", FILE_APPEND);
 }
