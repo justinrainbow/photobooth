c8df43d8a32b82f5c59de4c814de4d834868b688
Justin Rainbow <justin.rainbow@gmail.com>
2011-10-09 16:21:20 -0700

    Style updates

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ pubsub.subscribe("booth:capture");
 
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
+    // socket.emit('news', { hello: 'world' });
+    // socket.on('my other event', function (data) {
+    //     console.log(data);
+    // });
 });
 
 
